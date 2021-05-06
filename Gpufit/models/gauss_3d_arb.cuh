6583f4b6742440fb7750c5de38fafef3a32d9b85
Peter Brown <ptbrown1729@gmail.com>
2021-05-06 14:14:53 -0700

    changed expected user_data shape. Now have extra data to give the actual number of points to be used in each fit. This allows different fits to have different sizes.

--- Gpufit/models/gauss_3d_arb.cuh
+++ Gpufit/models/gauss_3d_arb.cuh
@@ -15,7 +15,11 @@ __device__ void calculate_gauss3d_arb(
 {
 
     // read coordinates stored in user data
-    // stored as arrays x1, y1, z1, x2, y2, z2, ...
+    // stored as arrays x1, y1, z1, x2, y2, z2, ... xn, yn, zn, num_1, ..., num_n
+    // the num_i allow the fits to be of different sizes. If so, image data and coordinates
+    // for each fit must still be padded to n_points, but only the first num_i <= n_points will be non-zero.
+
+    // extract coordinates
     // stride between points is 3*fit_index*n_points
     float * user_info_float = (float *) user_info;
     float x, y, z;
@@ -23,21 +27,42 @@ __device__ void calculate_gauss3d_arb(
     y = user_info_float[point_index + n_points + 3 * fit_index * n_points];
     z = user_info_float[point_index + 2*n_points + 3 * fit_index * n_points];
 
+    // extract sizes
+    // todo: probably better to store this as integers, but for a first test store as floats and cast
+    //int * user_info_int = (int *) user_info_float[3 * n_points * n_fits];
+    int nsize = (int) user_info_float[3 * n_points * n_fits + fit_index];
+
+    // extract parameters
     float const * p = parameters;
 
     // compute function value
-
-    value[point_index] =  p[6] + p[0] * expf(-0.5f * ((x - p[1]) * (x - p[1]) / p[4] / p[4] + (y - p[2]) * (y - p[2]) / p[4] / p[4] + (z - p[3]) * (z - p[3]) / p[5] / p[5]));
+    if (point_index < nsize){
+        value[point_index] =  p[6] + p[0] * expf(-0.5f * ((x - p[1]) * (x - p[1]) / p[4] / p[4] + (y - p[2]) * (y - p[2]) / p[4] / p[4] + (z - p[3]) * (z - p[3]) / p[5] / p[5]));
+	}
+    else {
+    	 value[point_index] = 0;
+	 }
 
     //compute function partial derivatives
-    
-    derivative[point_index + 0*n_points] = expf(-0.5f * ((x - p[1]) * (x - p[1]) / p[4] / p[4] + (y - p[2]) * (y - p[2]) / p[4] / p[4] + (z - p[3]) * (z - p[3]) / p[5] / p[5]));
-    derivative[point_index + 1*n_points] = p[0] * derivative[point_index] * (x - p[1]) / p[4] / p[4];
-    derivative[point_index + 2*n_points] = p[0] * derivative[point_index] * (y - p[2]) / p[4] / p[4];
-    derivative[point_index + 3*n_points] = p[0] * derivative[point_index] * (z - p[3]) / p[5] / p[5];
-    derivative[point_index + 4*n_points] = p[0] * derivative[point_index] * ((x - p[1]) * (x - p[1]) / p[4] / p[4] / p[4] + (y - p[2]) * (y - p[2]) / p[4] / p[4] / p[4]);
-    derivative[point_index + 5*n_points] = p[0] * derivative[point_index] * (z - p[3]) * (z - p[3]) / p[5] / p[5] / p[5];
-    derivative[point_index + 6*n_points] = 1.0;
+    if (point_index < nsize){
+        derivative[point_index + 0*n_points] = expf(-0.5f * ((x - p[1]) * (x - p[1]) / p[4] / p[4] + (y - p[2]) * (y - p[2]) / p[4] / p[4] + (z - p[3]) * (z - p[3]) / p[5] / p[5]));
+    	derivative[point_index + 1*n_points] = p[0] * derivative[point_index] * (x - p[1]) / p[4] / p[4];
+        derivative[point_index + 2*n_points] = p[0] * derivative[point_index] * (y - p[2]) / p[4] / p[4];
+    	derivative[point_index + 3*n_points] = p[0] * derivative[point_index] * (z - p[3]) / p[5] / p[5];
+	derivative[point_index + 4*n_points] = p[0] * derivative[point_index] * ((x - p[1]) * (x - p[1]) / p[4] / p[4] / p[4] + (y - p[2]) * (y - p[2]) / p[4] / p[4] / p[4]);
+	derivative[point_index + 5*n_points] = p[0] * derivative[point_index] * (z - p[3]) * (z - p[3]) / p[5] / p[5] / p[5];
+	derivative[point_index + 6*n_points] = 1.0;
+	}
+    else{
+	derivative[point_index + 0*n_points] = 0;
+	derivative[point_index + 1*n_points] = 0;
+	derivative[point_index + 2*n_points] = 0;
+	derivative[point_index + 3*n_points] = 0;
+	derivative[point_index + 4*n_points] = 0;
+	derivative[point_index + 5*n_points] = 0;
+	derivative[point_index + 6*n_points] = 0;
+	
+     	}
 
 }
 
