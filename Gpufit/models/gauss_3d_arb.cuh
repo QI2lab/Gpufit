709e672f82fefc7cf017c0df090c0661e058c3a8
ptbrown1729 <ptbrown1729@gmail.com>
2022-12-01 17:59:22 -0700

    added method for fixing a minimum value for Gaussian sigmas in 3D Gaussian fitting functions. Minimum sigmas values are the last arguments to user_info

--- Gpufit/models/gauss_3d_arb.cuh
+++ Gpufit/models/gauss_3d_arb.cuh
@@ -15,13 +15,15 @@ __device__ void calculate_gauss3d_arb(
 {
 
     // read coordinates stored in user data
-    // stored as arrays x1, y1, z1, x2, y2, z2, ... xn, yn, zn, num_1, ..., num_n
+    // stored as arrays x1, y1, z1, x2, y2, z2, ... xn, yn, zn, num_1, ..., num_n, sxy_min, sz_min
     // the num_i allow the fits to be of different sizes. If so, image data and coordinates
     // for each fit must still be padded to n_points, but only the first num_i <= n_points will be non-zero.
 
+    // cast user info to float
+    float * user_info_float = (float *) user_info;
+
     // extract coordinates
     // stride between points is 3*fit_index*n_points
-    float * user_info_float = (float *) user_info;
     float x, y, z;
     x = user_info_float[point_index + 3 * fit_index * n_points];
     y = user_info_float[point_index + n_points + 3 * fit_index * n_points];
@@ -32,36 +34,51 @@ __device__ void calculate_gauss3d_arb(
     //int * user_info_int = (int *) user_info_float[3 * n_points * n_fits];
     int nsize = (int) user_info_float[3 * n_points * n_fits + fit_index];
 
+    // extract minimum sigmas
+    float sxy_min = user_info_float[3 * n_points * n_fits + n_fits];
+    float sz_min = user_info_float[3 * n_points * n_fits + n_fits + 1];
+
     // extract parameters
     float const * p = parameters;
 
-    // compute function value
-    if (point_index < nsize){
-        value[point_index] =  p[6] + p[0] * expf(-0.5f * ((x - p[1]) * (x - p[1]) / p[4] / p[4] + (y - p[2]) * (y - p[2]) / p[4] / p[4] + (z - p[3]) * (z - p[3]) / p[5] / p[5]));
-	}
-    else {
-    	 value[point_index] = 0;
-	 }
-
     //compute function partial derivatives
     if (point_index < nsize){
-        derivative[point_index + 0*n_points] = expf(-0.5f * ((x - p[1]) * (x - p[1]) / p[4] / p[4] + (y - p[2]) * (y - p[2]) / p[4] / p[4] + (z - p[3]) * (z - p[3]) / p[5] / p[5]));
-    	derivative[point_index + 1*n_points] = p[0] * derivative[point_index] * (x - p[1]) / p[4] / p[4];
-        derivative[point_index + 2*n_points] = p[0] * derivative[point_index] * (y - p[2]) / p[4] / p[4];
-    	derivative[point_index + 3*n_points] = p[0] * derivative[point_index] * (z - p[3]) / p[5] / p[5];
-	derivative[point_index + 4*n_points] = p[0] * derivative[point_index] * ((x - p[1]) * (x - p[1]) / p[4] / p[4] / p[4] + (y - p[2]) * (y - p[2]) / p[4] / p[4] / p[4]);
-	derivative[point_index + 5*n_points] = p[0] * derivative[point_index] * (z - p[3]) * (z - p[3]) / p[5] / p[5] / p[5];
-	derivative[point_index + 6*n_points] = 1.0;
+        REAL const amp = p[0];
+        REAL const cx = p[1];
+        REAL const cy = p[2];
+        REAL const cz = p[3];
+        REAL const sxy = p[4];
+        REAL const sz = p[5];
+        REAL const bg = p[6];
+
+       REAL const ex = expf(-0.5f * ((x - cx) * (x - cx) / (sxy_min * sxy_min + sxy * sxy) +
+                                     (y - cy) * (y - cy) / (sxy_min * sxy_min + sxy * sxy) +
+                                     (z - cz) * (z - cz) / (sz_min * sz_min + sz * sz)
+                                     ));
+
+        // compute function value
+        value[point_index] =  bg + amp * ex;
+
+        // compute derivative value
+        derivative[point_index + 0*n_points] = ex;
+    	derivative[point_index + 1*n_points] = amp * ex * (x - cx) / (sxy_min * sxy_min + sxy * sxy);
+        derivative[point_index + 2*n_points] = amp * ex * (y - cy) / (sxy_min * sxy_min + sxy * sxy);
+    	derivative[point_index + 3*n_points] = amp * ex * (z - cz) / (sz_min * sz_min + sz * sz);
+	    derivative[point_index + 4*n_points] = amp * ex * (sxy / (sxy_min * sxy_min + sxy * sxy) / (sxy_min * sxy_min + sxy * sxy) * (x - cx) * (x - cx) +
+	                                                       sxy / (sxy_min * sxy_min + sxy * sxy) / (sxy_min * sxy_min + sxy * sxy) * (y - cy) * (y - cy));
+	    derivative[point_index + 5*n_points] = amp * ex * sz / (sz_min * sz_min + sz * sz) / (sz_min * sz_min + sz * sz) * (z - cz) * (z - cz);
+	    derivative[point_index + 6*n_points] = 1.0;
 	}
     else{
-	derivative[point_index + 0*n_points] = 0;
-	derivative[point_index + 1*n_points] = 0;
-	derivative[point_index + 2*n_points] = 0;
-	derivative[point_index + 3*n_points] = 0;
-	derivative[point_index + 4*n_points] = 0;
-	derivative[point_index + 5*n_points] = 0;
-	derivative[point_index + 6*n_points] = 0;
-	
+        value[point_index] = 0;
+
+	    derivative[point_index + 0*n_points] = 0;
+	    derivative[point_index + 1*n_points] = 0;
+	    derivative[point_index + 2*n_points] = 0;
+	    derivative[point_index + 3*n_points] = 0;
+	    derivative[point_index + 4*n_points] = 0;
+	    derivative[point_index + 5*n_points] = 0;
+	    derivative[point_index + 6*n_points] = 0;
      	}
 
 }
--- Gpufit/models/gauss_3d_rot_arb.cuh
+++ Gpufit/models/gauss_3d_rot_arb.cuh
@@ -13,15 +13,17 @@ __device__ void calculate_gauss3d_rot_arb(
     char * user_info,
     std::size_t const user_info_size)
 {
-    // read coordinates stored in user data
-    // stored as arrays x1, y1, z1, x2, y2, z2, ... xn, yn, zn, num_1, ..., num_n
+    // read coordinates stored in user_info
+    // stored as 1D raveled arrays: x1, y1, z1, x2, y2, z2, ... xn, yn, zn, num_1, ..., num_n, sx_min, sy_min, sz_min
     // the num_i allow the fits to be of different sizes. If so, image data and coordinates
     // for each fit must still be padded to n_points, but only the first num_i <= n_points will be non-zero.
 
-    // extract coordinates
-    // stride between points is 3*fit_index*n_points
+    // cast user_info to float
     float * user_info_float = (float *) user_info;
+
+    // extract coordinates
     float x, y, z;
+    // stride between points is 3*fit_index*n_points
     x = user_info_float[point_index + 3 * fit_index * n_points];
     y = user_info_float[point_index + n_points + 3 * fit_index * n_points];
     z = user_info_float[point_index + 2*n_points + 3 * fit_index * n_points];
@@ -30,6 +32,11 @@ __device__ void calculate_gauss3d_rot_arb(
     // todo: probably better to store this as integers, but for a first test store as floats and cast
     //int * user_info_int = (int *) user_info_float[3 * n_points * n_fits];
     int nsize = (int) user_info_float[3 * n_points * n_fits + fit_index];
+
+    // extract minimum sigmas
+    float sx_min = user_info_float[3 * n_points * n_fits + n_fits];
+    float sy_min = user_info_float[3 * n_points * n_fits + n_fits + 1];
+    float sz_min = user_info_float[3 * n_points * n_fits + n_fits + 2];
 
     // extract parameters [A, cx, cy, cz, sx, sy, sz, bg, phi, theta, psi]
     float const * p = parameters;
@@ -44,9 +51,22 @@ __device__ void calculate_gauss3d_rot_arb(
 
 
     if (point_index < nsize){
+        REAL const amp = p[0];
+        REAL const cx = p[1];
+        REAL const cy = p[2];
+        REAL const cz = p[3];
+        REAL const sx = p[4];
+        REAL const sy = p[5];
+        REAL const sz = p[6];
+        REAL const bg = p[7];
         REAL const phi = p[8];
         REAL const theta = p[9];
         REAL const psi = p[10];
+
+        // effective sigma^2 values
+        REAL const sx_sqr = sx_min * sx_min + sx * sx;
+        REAL const sy_sqr = sy_min * sy_min + sy * sy;
+        REAL const sz_sqr = sz_min * sz_min + sz * sz;
 
         // swapping phi/-psi, theta/-theta, psi/-phi will give us the inverse Euler matrix instead of the forward one, as desired
 
@@ -95,34 +115,39 @@ __device__ void calculate_gauss3d_rot_arb(
         REAL const dx22 = -1 * 0;
 
         // r_rot = R^{-1} * r
-        REAL const xrot = (x - p[1]) * r00 + (y - p[2]) * r01 + (z - p[3]) * r02;
-        REAL const yrot = (x - p[1]) * r10 + (y - p[2]) * r11 + (z - p[3]) * r12;
-        REAL const zrot = (x - p[1]) * r20 + (y - p[2]) * r21 + (z - p[3]) * r22;
-        REAL const ex = expf(-0.5f * (xrot * xrot / p[4] / p[4] + yrot * yrot / p[5] / p[5] + zrot * zrot / p[6] / p[6]));
+        REAL const xrot = (x - cx) * r00 + (y - cy) * r01 + (z - cz) * r02;
+        REAL const yrot = (x - cx) * r10 + (y - cy) * r11 + (z - cz) * r12;
+        REAL const zrot = (x - cx) * r20 + (y - cy) * r21 + (z - cz) * r22;
+        REAL const ex = expf(-0.5f * (xrot * xrot / sx_sqr +
+                                      yrot * yrot / sy_sqr +
+                                      zrot * zrot / sz_sqr
+                                      ));
 
         // compute function value
-        value[point_index] =  p[7] + p[0] * ex;
+        value[point_index] =  bg + amp * ex;
 
         //compute function partial derivatives
         derivative[point_index + 0*n_points] = ex;
         // df/dcx = df/dx * dxrot / dcx + df/dy * dyrot / dcx + df/dz * dzrot / dcx
-    	derivative[point_index + 1*n_points] = p[0] * ex * (xrot / p[4] / p[4] * r00 + yrot / p[5] / p[5] * r10 + zrot / p[6] / p[6] * r20);
-        derivative[point_index + 2*n_points] = p[0] * ex * (xrot / p[4] / p[4] * r01 + yrot / p[5] / p[5] * r11 + zrot / p[6] / p[6] * r21);
-    	derivative[point_index + 3*n_points] = p[0] * ex * (xrot / p[4] / p[4] * r02 + yrot / p[5] / p[5] * r12 + zrot / p[6] / p[6] * r22);
-	    derivative[point_index + 4*n_points] = p[0] * ex * xrot * xrot / p[4] / p[4] / p[4];
-	    derivative[point_index + 5*n_points] = p[0] * ex * yrot * yrot / p[5] / p[5] / p[5];
-	    derivative[point_index + 6*n_points] = p[0] * ex * zrot * zrot / p[6] / p[6] / p[6];
+    	derivative[point_index + 1*n_points] = amp * ex * (xrot / sx_sqr * r00 + yrot / sy_sqr * r10 + zrot / sz_sqr * r20);
+        derivative[point_index + 2*n_points] = amp * ex * (xrot / sx_sqr * r01 + yrot / sy_sqr * r11 + zrot / sz_sqr * r21);
+    	derivative[point_index + 3*n_points] = amp * ex * (xrot / sx_sqr * r02 + yrot / sy_sqr * r12 + zrot / sz_sqr * r22);
+	    // df/dsigma
+	    derivative[point_index + 4*n_points] = amp * ex * sx / sx_sqr * xrot * xrot;
+	    derivative[point_index + 5*n_points] = amp * ex * sy / sy_sqr * yrot * yrot;
+	    derivative[point_index + 6*n_points] = amp * ex * sz / sz_sqr * zrot * zrot;
+	    // df/dbackground
 	    derivative[point_index + 7*n_points] = 1;
 	    // df / dphi = df/dx * dxrot / dphi + df/dy * dyrot / dphi + df/dz * dzrot / dphi
-	    derivative[point_index + 8*n_points] = -p[0] * ex * (xrot / p[4] / p[4] * ((x - p[1]) * dp00 + (y - p[2]) * dp01  + (z - p[3]) * dp02) +
-	                                                         yrot / p[5] / p[5] * ((x - p[1]) * dp10 + (y - p[2]) * dp11  + (z - p[3]) * dp12) +
-	                                                         zrot / p[6] / p[6] * ((x - p[1]) * dp20 + (y - p[2]) * dp21  + (z - p[3]) * dp22));
-	    derivative[point_index + 9*n_points] = -p[0] * ex * (xrot / p[4] / p[4] * ((x - p[1]) * dt00 + (y - p[2]) * dt01  + (z - p[3]) * dt02) +
-	                                                         yrot / p[5] / p[5] * ((x - p[1]) * dt10 + (y - p[2]) * dt11  + (z - p[3]) * dt12) +
-	                                                         zrot / p[6] / p[6] * ((x - p[1]) * dt20 + (y - p[2]) * dt21  + (z - p[3]) * dt22));
-	    derivative[point_index + 10*n_points] = -p[0] * ex * (xrot / p[4] / p[4] * ((x - p[1]) * dx00 + (y - p[2]) * dx01  + (z - p[3]) * dx02) +
-	                                                         yrot / p[5] / p[5] * ((x - p[1]) * dx10 + (y - p[2]) * dx11  + (z - p[3]) * dx12) +
-	                                                         zrot / p[6] / p[6] * ((x - p[1]) * dx20 + (y - p[2]) * dx21  + (z - p[3]) * dx22));
+	    derivative[point_index + 8*n_points] =  -amp * ex * (xrot / sx_sqr * ((x - cx) * dp00 + (y - cy) * dp01  + (z - cz) * dp02) +
+	                                                         yrot / sy_sqr * ((x - cx) * dp10 + (y - cy) * dp11  + (z - cz) * dp12) +
+	                                                         zrot / sz_sqr * ((x - cx) * dp20 + (y - cy) * dp21  + (z - cz) * dp22));
+	    derivative[point_index + 9*n_points] =  -amp * ex * (xrot / sx_sqr * ((x - cx) * dt00 + (y - cy) * dt01  + (z - cz) * dt02) +
+	                                                         yrot / sy_sqr * ((x - cx) * dt10 + (y - cy) * dt11  + (z - cz) * dt12) +
+	                                                         zrot / sz_sqr * ((x - cx) * dt20 + (y - cy) * dt21  + (z - cz) * dt22));
+	    derivative[point_index + 10*n_points] = -amp * ex * (xrot / sx_sqr * ((x - cx) * dx00 + (y - cy) * dx01  + (z - cz) * dx02) +
+	                                                         yrot / sy_sqr * ((x - cx) * dx10 + (y - cy) * dx11  + (z - cz) * dx12) +
+	                                                         zrot / sz_sqr * ((x - cx) * dx20 + (y - cy) * dx21  + (z - cz) * dx22));
 
 	}
     else {
